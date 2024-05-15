6e79f2743a54110abbe92a9a616766150f22405f
xenlon <>
2024-05-15 18:29:39 +0530

    Added collec uri

--- entrypoint.py
+++ entrypoint.py
@@ -17,7 +17,6 @@ def record_engine_temperature():
     logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
 
     engine_temperature = payload.get("engine_temperature")
-    current_engine_temperature = engine_temperature
     logger.info(f"engine temperature to record is: {engine_temperature}")
 
     database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
@@ -27,13 +26,20 @@ def record_engine_temperature():
     while database.llen(DATA_KEY) > HISTORY_LENGTH:
         database.rpop(DATA_KEY)
     engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+
+    logger.info(f"record request successful")
+    return {"success": True}, 200
+
+@app.route('/collect', methods=['POST'])
+def collect_engine_temperature():
     total_temperature = 0
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    current_engine_temperature = engine_temperature_values[0]
 
     for i in engine_temperature_values:
         total_temperature += i
-    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
     average_engine_temperature = total_temperature/len(engine_temperature_values)
 
-    logger.info(f"record request successful")
     return {"current_engine_temperature": current_engine_temperature,
             "average_engine_tmperature": average_engine_temperature}, 200
